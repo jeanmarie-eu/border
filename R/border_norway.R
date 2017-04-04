4e4292ae861c7d84447cad13a46137b3044aa617
jeanmarielepioufle <jm@nexmodeling.com>
2017-04-04 11:10:56 +0200

    made public

---
+++ R/border_norway.R
@@ -0,0 +1,11 @@
+#' Norwegian border
+#'
+#' Norwegian border
+#'
+#' @docType data
+#' @name border_norway
+#' @format A rda file
+#' @keywords datasets
+#' @references to be added
+#' @source to be added
+NULL
---
+++ R/border_world.R
@@ -0,0 +1,11 @@
+#' World border
+#'
+#' World border
+#'
+#' @docType data
+#' @name border_world
+#' @format A rda file
+#' @keywords datasets
+#' @references to be added
+#' @source to be added
+NULL
